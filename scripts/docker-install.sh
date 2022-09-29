0b0064e5a0fccb775ce7282883a0f415310b8db8
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2022-09-29 09:34:11 +0530

    Update docker-install.sh

--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 docker pull ghcr.io/middleware-labs/agent-host-go:dev
 docker run -d \
---name mw-agent-${MW_API_KEY:0:5}
+--name mw-agent-${MW_API_KEY:0:5} \
 --pid host \
 --restart always \
 -e MW_API_KEY=$MW_API_KEY \
