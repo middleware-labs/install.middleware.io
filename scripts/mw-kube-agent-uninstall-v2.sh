236d54408ad655c6c31bf7e39a10da219c81268e
Keval Bhogayata <90185475+bhogayatakb@users.noreply.github.com>
2024-03-29 16:13:43 +0530

    Modified k8s agent v2 script (#170)

--- scripts/mw-kube-agent-uninstall-v2.sh
+++ scripts/mw-kube-agent-uninstall-v2.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e errexit
-LOG_FILE="/var/log/mw-kube-agent/mw-kube-agent-install-$(date +%s).log"
+LOG_FILE="/var/log/mw-kube-agent/mw-kube-agent-uninstall-$(date +%s).log"
 sudo mkdir -p /var/log/mw-kube-agent
 sudo touch "$LOG_FILE"
 exec &> >(sudo tee -a "$LOG_FILE")
@@ -13,7 +13,7 @@ function send_logs {
 {
   "status": "$status",
   "metadata": {
-    "script": "kubernetes",
+    "script": "kubernetes-v2",
     "status": "ok",
     "message": "$message",
     "script_logs": "$(sed 's/$/\\n/' "$LOG_FILE" | tr -d '\n' | sed 's/"/\\\"/g' | sed 's/\t/\\t/g')"
@@ -29,9 +29,9 @@ curl -s --location --request POST https://app.middleware.io/api/v1/agent/trackin
 
 function on_exit {
   if [ $? -eq 0 ]; then
-    send_logs "installed" "Script Completed"
+    send_logs "success" "uninstall completed"
   else
-    send_logs "error" "Script Failed"
+    send_logs "error" "uninstall failed"
   fi
 }
 
@@ -45,7 +45,7 @@ curl -s --location --request POST https://app.middleware.io/api/v1/agent/trackin
     "metadata": {
         "script": "kubernetes",
         "status": "ok",
-        "message": "agent installed"
+        "message": "agent uninstalled"
     }
 }' > /dev/null
 
@@ -53,24 +53,8 @@ curl -s --location --request POST https://app.middleware.io/api/v1/agent/trackin
 MW_DEFAULT_NAMESPACE=mw-agent-ns
 export MW_DEFAULT_NAMESPACE
 
-MW_DEFAULT_API_URL_FOR_CONFIG_CHECK=http://app.middleware.io
-export MW_DEFAULT_API_URL_FOR_CONFIG_CHECK
-
-MW_DEFAULT_CONFIG_CHECK_INTERVAL="*/1 * * * *"
-export MW_DEFAULT_CONFIG_CHECK_INTERVAL
-
 if [ "${MW_NAMESPACE}" = "" ]; then 
   MW_NAMESPACE=$MW_DEFAULT_NAMESPACE
-  export MW_NAMESPACE
-fi
-
-if [ "${MW_API_URL_FOR_CONFIG_CHECK}" = "" ]; then 
-  MW_API_URL_FOR_CONFIG_CHECK=$MW_DEFAULT_API_URL_FOR_CONFIG_CHECK
-  export MW_NAMESPACE
-fi
-
-if [ "${MW_CONFIG_CHECK_INTERVAL}" = "" ]; then 
-  MW_CONFIG_CHECK_INTERVAL=$MW_DEFAULT_CONFIG_CHECK_INTERVAL
   export MW_NAMESPACE
 fi
 
@@ -79,10 +63,9 @@ CURRENT_CONTEXT="$(kubectl config current-context)"
 MW_KUBE_CLUSTER_NAME="$(kubectl config view -o jsonpath="{.contexts[?(@.name == '"$CURRENT_CONTEXT"')].context.cluster}")"
 export MW_KUBE_CLUSTER_NAME
 
-echo -e "\nSetting up Middleware Kubernetes agent ...\n\n\tcluster : $MW_KUBE_CLUSTER_NAME \n\tcontext : $CURRENT_CONTEXT\n"
+echo -e "\nUninstalling Middleware Kubernetes agent ...\n\n\tcluster : $MW_KUBE_CLUSTER_NAME \n\tcontext : $CURRENT_CONTEXT\n"
 
-
-echo -e "\nMiddleware Kubernetes agent is being installed using manifest files, please wait ..."
+echo -e "\nMiddleware Kubernetes agent is being uninstalled using manifest files, please wait ..."
 # Home for local configs
 MW_KUBE_AGENT_HOME=/usr/local/bin/mw-kube-agent
 export MW_KUBE_AGENT_HOME
@@ -94,25 +77,21 @@ cp -r ./mw-kube-agent/* $MW_KUBE_AGENT_HOME
 ls -l $MW_KUBE_AGENT_HOME
 EOSUDO
 
-kubectl --kubeconfig "${MW_KUBECONFIG}" create namespace ${MW_NAMESPACE}
-sudo wget -q -O otel-config-deployment.yaml https://install.middleware.io/scripts/otel-config-deployment.yaml
-sudo wget -q -O otel-config-daemonset.yaml https://install.middleware.io/scripts/otel-config-daemonset.yaml
-kubectl --kubeconfig "${MW_KUBECONFIG}" create configmap mw-deployment-otel-config --from-file=otel-config=otel-config-deployment.yaml --namespace=${MW_NAMESPACE}
-kubectl --kubeconfig "${MW_KUBECONFIG}" create configmap mw-daemonset-otel-config --from-file=otel-config=otel-config-daemonset.yaml --namespace=${MW_NAMESPACE}     
 for file in "$MW_KUBE_AGENT_HOME"/*.yaml; do
-  kubectl apply -f <( \
+  kubectl delete -f <( \
     cat "$file" | \
     sed -e "s|MW_KUBE_CLUSTER_NAME_VALUE|${MW_KUBE_CLUSTER_NAME}|g" \
-        -e "s|MW_ROLLOUT_RESTART_RULE|${MW_ROLLOUT_RESTART_RULE}|g" \
-        -e "s|MW_LOG_PATHS|${MW_LOG_PATHS}|g" \
-        -e "s|MW_DOCKER_ENDPOINT_VALUE|${MW_DOCKER_ENDPOINT}|g" \
-        -e "s|MW_API_KEY_VALUE|${MW_API_KEY}|g" \
-        -e "s|TARGET_VALUE|${MW_TARGET}|g" \
         -e "s|NAMESPACE_VALUE|${MW_NAMESPACE}|g" \
         -e "s|MW_API_URL_FOR_CONFIG_CHECK_VALUE|${MW_API_URL_FOR_CONFIG_CHECK}|g" \
         -e "s|MW_CONFIG_CHECK_INTERVAL_VALUE|${MW_CONFIG_CHECK_INTERVAL}|g" \
   ) --kubeconfig "${MW_KUBECONFIG}"
 done
 
-echo "Middleware Kubernetes agent successfully installed !"
+kubectl --kubeconfig "${MW_KUBECONFIG}" delete configmap mw-deployment-otel-config --namespace=${MW_NAMESPACE}
+kubectl --kubeconfig "${MW_KUBECONFIG}" delete configmap mw-daemonset-otel-config --namespace=${MW_NAMESPACE}
+kubectl --kubeconfig "${MW_KUBECONFIG}" delete namespace ${MW_NAMESPACE}
+
+
+
+echo "Middleware Kubernetes agent successfully uninstalled !"
 
