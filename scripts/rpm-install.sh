512b036a1c4c37d1920fae6b541b99ccff4e44bc
Tejas Kokje <136300685+tejaskokje-mw@users.noreply.github.com>
2024-04-29 23:17:14 -0700

    Cleanup debian and rpm installation scripts (#182)
    * Cleanup debian and rpm installation scripts
    
    * Add alma linux, rocky linux, amazon, oraclelinux and suse support for rpm installation

--- scripts/rpm-install.sh
+++ scripts/rpm-install.sh
@@ -6,7 +6,7 @@ command_exists() {
 }
 
 # Check if required commands are available
-required_commands=("sudo" "mkdir" "touch" "exec" "tee" "date" "curl" "uname" "source" "sed" "tr" "wget" "systemctl" "chmod" "rpm")
+required_commands=("sudo" "mkdir" "touch" "exec" "tee" "date" "curl" "uname" "source" "sed" "tr" "systemctl" "chmod" "rpm")
 missing_commands=()
 
 for cmd in "${required_commands[@]}"; do
@@ -25,7 +25,11 @@ fi
 LOG_FILE="/var/log/mw-agent/rpm-installation-$(date +%s).log"
 sudo mkdir -p /var/log/mw-agent
 sudo touch "$LOG_FILE"
-exec &> >(sudo tee -a "$LOG_FILE")
+
+MW_TRACKING_TARGET="https://app.middleware.io"
+if [ -n "$MW_API_URL_FOR_CONFIG_CHECK" ]; then
+    export MW_TRACKING_TARGET="$MW_API_URL_FOR_CONFIG_CHECK"
+fi
 
 function send_logs {
   status=$1
@@ -51,6 +55,23 @@ EOF
   --data "$payload" > /dev/null
 }
 
+function force_continue {
+  read -p "Do you still want to continue? (y|N): " response
+  case "$response" in
+    [yY])
+      echo "Continuing with the script..."
+      ;;
+    [nN])
+      echo "Exiting script..."
+      exit 1
+      ;;
+    *)
+      echo "Invalid input. Please enter 'yes' or 'no'."
+      force_continue # Recursively call the function until valid input is received.
+      ;;
+  esac
+}
+
 function on_exit {
   if [ $? -eq 0 ]; then
     send_logs "installed" "Script Completed"
@@ -62,159 +83,115 @@ function on_exit {
 trap on_exit EXIT
 
 # recording agent installation attempt
-curl -s --location --request POST https://app.middleware.io/api/v1/agent/tracking/$MW_API_KEY \
---header 'Content-Type: application/json' \
---data '{
-    "status": "tried",
-    "metadata": {
-        "script": "linux-rpm",
-        "status": "ok",
-        "message": "agent installed"
-    }
-}' > /dev/null
+# recording agent installation attempt
+send_logs "tried" "Agent Installation Attempted"
 
-MW_AGENT_HOME=/usr/local/bin/mw-agent
-MW_AGENT_BINARY=mw-agent
-MW_DETECTED_ARCH=$(uname -m)
+# Check if the system is running Linux
+if [ "$(uname -s)" != "Linux" ]; then
+  echo "This machine is not running Linux, The script is designed to run on a Linux machine."
+  force_continue
+fi
 
-RPM_FILE=""
-
-echo -e "\n'"$MW_DETECTED_ARCH"' architecture detected ..."
-
-MW_LATEST_VERSION="1.5.0"
+MW_LATEST_VERSION="1.6.4"
 export MW_LATEST_VERSION
 
+# Check if MW_VERSION is provided
 if [ "${MW_VERSION}" = "" ]; then 
   MW_VERSION=$MW_LATEST_VERSION
-  export MW_VERSION
 fi
+export MW_VERSION
+echo -e "\nInstalling Middleware Agent version ${MW_VERSION} on hostname $(hostname) at $(date)" | sudo tee -a "$LOG_FILE"
 
-MW_DEFAULT_SKIP_CERTIFICATE_CHECK="no"
-if [ "${MW_SKIP_CERTIFICATE_CHECK}" = "" ]; then 
-  MW_SKIP_CERTIFICATE_CHECK=$MW_DEFAULT_SKIP_CERTIFICATE_CHECK
-  export MW_SKIP_CERTIFICATE_CHECK
-fi
-
-if [[ $MW_DETECTED_ARCH == "x86_64" ]]; then
-  RPM_FILE="mw-agent-${MW_VERSION}-1.x86_64.rpm"
-  MW_AGENT_BINARY="mw-agent"
-elif [[ $MW_DETECTED_ARCH == "aarch64" ]]; then
-  RPM_FILE="mw-agent-${MW_VERSION}-1.aarch64.rpm"
-  MW_AGENT_BINARY="mw-agent"
+# Check if /etc/os-release file exists
+if [ -f /etc/os-release ]; then
+  source /etc/os-release
+  case "$ID" in
+    rhel|centos|fedora|almalinux|rockylinux|amzn|ol|sles)
+      echo "os-release ID is $ID"
+      ;;
+    *)
+      case "$ID_LIKE" in
+        rhel|centos|fedora|suse)
+          echo  "os-release ID_LIKE is $ID_LIKE"
+          ;;
+        *)
+          echo "This is not a RPM based Linux distribution."
+          force_continue
+          ;;
+      esac
+  esac
 else
-  echo ""
+  echo "/etc/os-release file not found. Unable to determine the distribution."
+  force_continue
 fi
 
-echo "yum.middleware.io/$MW_DETECTED_ARCH/Packages/$RPM_FILE"
-if [[ $MW_SKIP_CERTIFICATE_CHECK == "yes" ]]; then
-  wget -q -O $RPM_FILE yum.middleware.io/$MW_DETECTED_ARCH/Packages/$RPM_FILE --no-check-certificate
+if [ "${MW_DETECTED_ARCH}" = "" ]; then 
+  MW_DETECTED_ARCH=$(uname -m)
+  echo -e "cpu architecture detected: '"${MW_DETECTED_ARCH}"'"
 else 
-  wget -q -O $RPM_FILE yum.middleware.io/$MW_DETECTED_ARCH/Packages/$RPM_FILE 
+  echo -e "cpu architecture provided: '"${MW_DETECTED_ARCH}"'"
 fi
-sudo rpm -i $RPM_FILE
-export PATH=$PATH:/usr/bin/$MW_AGENT_BINARY
-source ~/.bashrc
+export MW_DETECTED_ARCH
 
-export MW_AUTO_START=true
+RPM_FILE="mw-agent-${MW_VERSION}-1.${MW_DETECTED_ARCH}.rpm"
 
-MW_LOG_PATHS=""
+
+if [ "${MW_AGENT_HOME}" = "" ]; then 
+  MW_AGENT_HOME=/opt/mw-agent
+fi
+export MW_AGENT_HOME
+
+MW_AGENT_BINARY=mw-agent
+if [ "${MW_AGENT_BINARY}" = "" ]; then 
+  MW_AGENT_BINARY=mw-agent
+fi
+
+if [ "${MW_AUTO_START}" = "" ]; then 
+  MW_AUTO_START=true
+fi
+export MW_AUTO_START
+
+if [ "${MW_API_KEY}" = "" ]; then 
+  echo "MW_API_KEY environment variable is required and is not set."
+  force_continue
+fi
+export MW_API_KEY
+
+if [ "${MW_TARGET}" = "" ]; then 
+  echo "MW_TARGET environment variable is required and is not set."
+  force_continue
+fi
+export MW_TARGET
+
+skip_certificate_check=""
+if [ "${MW_SKIP_CERTIFICATE_CHECK}" = "yes" ]; then 
+  skip_certificate_check="--no-check-certificate"
+fi
 
 echo -e "\nThe host agent will monitor all '.log' files inside your /var/log directory recursively [/var/log/**/*.log]\n"
-echo -e "Downloading data ingestion rules ...\n"
-sudo mkdir -p /usr/bin/$MW_AGENT_BINARY/configyamls/all
-sudo wget -q -O /usr/bin/$MW_AGENT_BINARY/configyamls/all/otel-config.yaml https://install.middleware.io/configyamls/all/otel-config.yaml
-sudo mkdir -p /usr/bin/$MW_AGENT_BINARY/configyamls/nodocker
-sudo wget -q -O /usr/bin/$MW_AGENT_BINARY/configyamls/nodocker/otel-config.yaml https://install.middleware.io/configyamls/nodocker/otel-config.yaml
+echo "yum.middleware.io/$MW_DETECTED_ARCH/Packages/$RPM_FILE"
+curl -L -q -s -o $RPM_FILE yum.middleware.io/$MW_DETECTED_ARCH/Packages/$RPM_FILE $skip_certificate_check
 
-sudo chmod 777 /usr/bin/$MW_AGENT_BINARY/configyamls/all/otel-config.yaml
-sudo chmod 777 /usr/bin/$MW_AGENT_BINARY/configyamls/nodocker/otel-config.yaml
+echo -e "Installing Middleware Agent Service ...\n"
 
-
-MW_USER=$(whoami)
-export MW_USER
-
-sudo su << EOSUDO
-
-
-# Running Agent as a Daemon Service
-touch /etc/systemd/system/mwservice.service
-mkdir -p $MW_AGENT_HOME/apt 
-touch $MW_AGENT_HOME/apt/executable
-
-cat << EOF > /etc/systemd/system/mwservice.service
-[Unit]
-Description=Melt daemon!
-[Service]
-User=$MW_USER
-#Code to execute
-#Can be the path to an executable or code itself
-WorkingDirectory=$MW_AGENT_HOME/apt
-ExecStart=$MW_AGENT_HOME/apt/executable
-Type=simple
-TimeoutStopSec=10
-Restart=on-failure
-RestartSec=5
-[Install]
-WantedBy=multi-user.target
-EOF
-
-
-cat << EOEXECUTABLE > $MW_AGENT_HOME/apt/executable
-#!/bin/sh
-
-# Check if MW_API_KEY is non-empty, then set the environment variable
-if [ -n "$MW_API_KEY" ]; then
-    export MW_API_KEY="$MW_API_KEY"
+sudo -E rpm -U $RPM_FILE
+# Check for errors
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to install Middleware Agent."
+  exit $?
 fi
-
-# Check if MW_TARGET is non-empty, then set the environment variable
-if [ -n "$MW_TARGET" ]; then
-    export MW_TARGET="$MW_TARGET"
-fi
-
-# Check if MW_ENABLE_SYNTHETIC_MONITORING is non-empty, then set the environment variable
-if [ -n "$MW_ENABLE_SYNTHETIC_MONITORING" ]; then
-    export MW_ENABLE_SYNTHETIC_MONITORING="$MW_ENABLE_SYNTHETIC_MONITORING"
-fi
-
-# Check if MW_CONFIG_CHECK_INTERVAL is non-empty, then set the environment variable
-if [ -n "$MW_CONFIG_CHECK_INTERVAL" ]; then
-    export MW_CONFIG_CHECK_INTERVAL="$MW_CONFIG_CHECK_INTERVAL"
-fi
-
-# Check if MW_DOCKER_ENDPOINT is non-empty, then set the environment variable
-if [ -n "$MW_DOCKER_ENDPOINT" ]; then
-    export MW_DOCKER_ENDPOINT="$MW_DOCKER_ENDPOINT"
-fi
-
-# Check if MW_API_URL_FOR_CONFIG_CHECK is non-empty, then set the environment variable
-if [ -n "$MW_API_URL_FOR_CONFIG_CHECK" ]; then
-    export MW_API_URL_FOR_CONFIG_CHECK="$MW_API_URL_FOR_CONFIG_CHECK"
-fi
-
-# Check if MW_HOST_TAGS is non-empty, then set the environment variable
-if [ -n "$MW_HOST_TAGS" ]; then
-    export MW_HOST_TAGS="$MW_HOST_TAGS"
-fi
-
-# Start the MW_AGENT_BINARY with the configured environment variables
-export PATH=$PATH:/usr/bin/$MW_AGENT_BINARY
-$MW_AGENT_BINARY start
-
-EOEXECUTABLE
-
-mkdir -p /etc/mw-agent
-touch /etc/mw-agent/otel-config.yaml
-chmod 777 $MW_AGENT_HOME/apt/executable
-chmod 777 /etc/mw-agent/otel-config.yaml
-
-EOSUDO
 
 sudo systemctl daemon-reload
-sudo systemctl enable mwservice
 
-if [ "${MW_AUTO_START}" = true ]; then	
-    sudo systemctl start mwservice
+sudo systemctl enable mw-agent
+#check for errors
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to enable Middleware Agent service."
+  exit $?
 fi
 
-echo -e "Installation done ! \n"
+if [ "${MW_AUTO_START}" = true ]; then
+    sudo systemctl start mw-agent
+fi
+
+echo -e "Middleware Agent installation completed successfully.\n"
