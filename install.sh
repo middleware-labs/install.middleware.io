770599c1fecd94362d97e4be199400d5ac1e31d2
naman-jain-15 <naman.jain@middleware.io>
2024-11-25 16:52:32 +0530

    fix

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $FOOO   # ShellCheck issue: Double quote to prevent globbing
+echo $FOO   # ShellCheck issue: Double quote to prevent globbing
 cd $(pwd)   # ShellCheck issue: Double quote to prevent globbing
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo $FOOO   # ShellCheck issue: Double quote to prevent globbing
+echo $FOODO   # ShellCheck issue: Double quote to prevent globbing
 cd $(pwd)   # ShellCheck issue: Double quote to prevent globbing
