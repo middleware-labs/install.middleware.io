ad25da942935279cfd3bf7d89cf02a49dd1200db
naman47vyas <naman.vyas@middleware.io>
2026-07-27 11:45:39 +0530

    corrected failing shell check in deb_install_injector.sh

--- scripts/deb_install_injector.sh
+++ scripts/deb_install_injector.sh
@@ -139,6 +139,7 @@ echo -e "\nInstalling Middleware Agent version ${MW_VERSION} on hostname $(hostn
 
 # Check if /etc/os-release file exists
 if [ -f /etc/os-release ]; then
+  # shellcheck source=/dev/null
   source /etc/os-release
   case "$ID" in
     debian|ubuntu)
